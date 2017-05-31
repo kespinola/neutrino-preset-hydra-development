d2a3ac99436fef953a46af8f1f15799397f79b1f
Kyle Espinola <ksespinola@gmail.com>
2017-05-30 17:38:39 -0700

    no chunk for dev

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ module.exports = neutrino => {
   neutrino
     .config
       .when(process.env.NODE_ENV === 'production', config => {
-        config.plugins.delete('minify')
+        config.plugins
+          .delete('minify')
+          .delete('chunk')
 
         config.output.filename('[name].bundle.js')
       })
