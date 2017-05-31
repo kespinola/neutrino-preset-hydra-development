19bdd9c265550129735c3882ef4ebe35dc1dfc9c
Kyle Espinola <ksespinola@gmail.com>
2017-05-30 19:01:33 -0700

    1.0.2

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ module.exports = neutrino => {
   neutrino
     .config
       .when(process.env.NODE_ENV === 'production', config => {
-        config.plugins
-          .delete('minify')
-          .delete('chunk')
+        config.plugins.delete('minify')
+        config.plugins.delete('chunk')
 
         config.output.filename('[name].bundle.js')
       })
