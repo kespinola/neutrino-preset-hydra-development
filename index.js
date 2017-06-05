5a4decff0bcb51554db3a7acbd93071321e02d06
Kyle Espinola <ksespinola@gmail.com>
2017-06-05 14:07:13 -0700

    move output filename rename to base neutrino config.

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ module.exports = neutrino => {
       .when(process.env.NODE_ENV === 'production', config => {
         config.plugins.delete('minify')
         config.plugins.delete('chunk')
-
-        config.output.filename('[name].bundle.js')
       })
       .when(process.env.NODE_ENV == 'development', config => {
         config.plugins.delete('chunk')
